20eaa4350f1d59b060fe16565c21505c491e8580
Anup Dhoble <83175840+anupdhoble@users.noreply.github.com>
2024-01-13 19:32:52 +0530

    pasword hashing , alt,pepper done jwt token inittiated,   jwt token decrpyption, verification pending , will do in route :3 (getUser)

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 
 const { Users } = require('../models/Users');
+const bcrypt = require('bcryptjs');
 
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-
+const JWT_SECRET = "I am Anup";
 
 router.get('/getAll', async (req, res) => {
     const users = await Users.find();
@@ -14,7 +16,7 @@ router.get('/getAll', async (req, res) => {
 router.post('/new', async (req, res) => {
     try {
         if (
-            !req.body.name||
+            !req.body.name ||
             !req.body.email ||
             !req.body.password
         ) {
@@ -22,24 +24,85 @@ router.post('/new', async (req, res) => {
                 message: 'Send all required fields: name,email,password',
             });
         }
+
+
+        const salt = await bcrypt.genSalt(10);
+        const securePassword = await bcrypt.hash(req.body.password, salt);
+
+
+
         const newUser = {
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-            date:req.body.date
+            password: securePassword,
+            date: req.body.date
         };
 
 
         const user = await Users.create(newUser);
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+
+
+
         return res.status(201).json({
             success: true,
-            user
+            authToken
         });
     } catch (error) {
         console.log(error.message);  // Fix the typo here
-        return res.status(500).send({ message: error.message });
+        return res.status(500).send("Internal Server Error");
     }
 });
 
+//Authenticate User
+router.post('/auth', async(req, res) => {
+    try {
+        if (
+            !req.body.email ||
+            !req.body.password
+        ) {
+            return res.status(400).send({
+                message: 'Send all required fields: email,password',
+            });
+        }
 
-module.exports =router;
+        const { email, password } = req.body;
+        let user = await Users.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Try Logging with correct credentials" });
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Try Logging with correct credentials" });
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        return res.status(201).json({
+            success: true,
+            authToken
+        });
+
+    } catch (error) {
+        console.log(error.message);  // Fix the typo here
+        return res.status(500).send("Internal Server Error");
+    }
+})
+
+//Getuser details with token provided. Login required
+
+
+
+module.exports = router;
